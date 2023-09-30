d1f54c10885854d713f750c1186ba9dd75bd4b61
nirajpatidar <nirajpatidar11@gmail.com>
2023-09-30 17:23:55 +0530

    express route first assignment

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middle ware!');
-    next(); // Allow to request continue the next middleware in line
+// app.use((req, res, next) => {
+//     console.log('This is first middle ware');
+//     next(); // Allow to request continue the next middleware in line
+// })
+// app.use((req, res, next) => {
+//     console.log('This is second middleware');
+//     // we send the response here
+//     res.send('<h1>Hello Welcome to the express js !</h1>');
+// })
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>It handle the /users route</h1>')
 })
-app.use((req, res, next) => {
-    console.log('In the next middle ware!');
-    // we send the rsponse here
-    res.send('<h1>Hello Welcome to the express js !</h1>');
-    // express js middleware executed top to bottom
-    // if we not call the next() then next middle ware not executed
-    //  default res header type is html
+app.use('/', (req, res, next) => {
+    res.send('<h1>It handle the / request</h1>')
 })
 app.listen(3000)
