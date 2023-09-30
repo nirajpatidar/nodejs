ecb3cac55194c62d4bd98b71f8830cbec1d47ac4
nirajpatidar <nirajpatidar11@gmail.com>
2023-09-30 17:17:38 +0530

    add route middleware

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middle ware!');
-    next(); // Allow to request continue the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This middleware always runs')
+    next();
+ })
+app.use('/add-product', (req, res, next) => {
+   res.send('<h1>this is Add product page!</h1>')
 })
-app.use((req, res, next) => {
-    console.log('In the next middle ware!');
-    // we send the rsponse here
-    res.send('<h1>Hello Welcome to the express js !</h1>');
-    // express js middleware executed top to bottom
-    // if we not call the next() then next middle ware not executed
-    //  default res header type is html
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome on root page</h1>')
 })
 app.listen(3000)
