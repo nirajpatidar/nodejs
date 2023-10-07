64d0116ab87a080b8ef317bfdefeb6557bdc000f
nirajpatidar <nirajpatidar11@gmail.com>
2023-10-07 15:05:16 +0530

    serve static file and add view html pages

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop') ;
-
+const shopRouter = require('./routes/shop');
+const rootDir = require('./util/path');
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminRouter)
 app.use(shopRouter)
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    // res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 // access from /admin GET
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>'
-    );
+    // res.send(
+    //     '<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add</button></form>'
+    // );
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 // access from /admin POST
 router.post('/product', (req, res, next) => {
