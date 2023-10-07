023abb4e59daa0ee43c7348da7b20107eb5a886c
nirajpatidar <nirajpatidar11@gmail.com>
2023-10-07 15:28:45 +0530

    navigation and serve static file assignment

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
-const rootDir = require('./util/path');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+const shopRouter = require('./routes/shop');
+app.use(shopRouter);
+app.listen(3000);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(rootDir, 'public')));
-
-app.use('/admin', adminRouter)
-app.use(shopRouter)
-app.use((req, res, next) => {
-    // res.status(404).send('<h1>Page not found</h1>')
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-})
-app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,10 @@ const router = express.Router();
 // when ever we use get, post etc method then it automatically exact match
 // access from / get
 router.get('/', (req, res, next) => {
-    // res.send('<h1>Welcome on root page</h1>')
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.sendFile(path.join(rootDir, 'views', 'main.html'))
+})
+router.get('/users', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'user.html'))
 })
 
 module.exports = router;
